cede62e20033a716414cffd7b05eeec71e4692b0
Adrian Kosmaczewski <adrian.kosmaczewski@vshn.ch>
2022-08-30 09:18:50 +0200

    Updated to latest version

--- signal-listener.sh
+++ signal-listener.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# A wrapper around /entrypoint.sh to trap the SIGINT signal (Ctrl+C) and forwards it to the mysql daemon
+# A wrapper around /entrypoint.sh to trap the SIGINT signal (Ctrl+C) and forwards it to the background daemon
 # In other words : traps SIGINT and SIGTERM signals and forwards them to the child process as SIGTERM signals
 # https://medium.com/@manish_demblani/docker-container-uncaught-kill-signal-d5ed22698293
 
 signalListener() {
     "$@" &
     pid="$!"
-    trap "echo 'Stopping PID $pid'; kill -SIGTERM $pid" SIGINT SIGTERM
+    trap "echo 'Stopping PID $pid'; kill -SIGTERM $pid; caddy stop" SIGINT SIGTERM
 
     # A signal emitted while waiting will make the wait command return code > 128
     # Let's wrap it in a loop that doesn't end before the process is indeed stopped
